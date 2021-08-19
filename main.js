bcf3afaf4735d27158030d611d228ae2b3d1b253
nicojpalacio <nicojpalacio@gmail.com>
2021-08-19 16:24:16 -0300

    responsite semi-arreglado

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 document.querySelector(".menu-btn").addEventListener("click", () => {
     document.querySelector(".nav-menu").classList.toggle("show");
   });
+
+  function ocultarTexto(){
+    document.getElementById("parrafo").classList.toggle("line-clamp")
+    masInfo();
+  }
+
+  function masInfo(){
+    document.getElementById("button").innerHTML= "Menos Info";
+  }
